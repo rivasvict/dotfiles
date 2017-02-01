0ee1edab86f098fb5c8e4326a66d07e54e5c792d
Victor Rivas <vctr90w@gmail.com>
2017-02-01 05:12:59 -0400

    Installation file done

---
+++ README.md
@@ -0,0 +1,16 @@
+﻿ENVIRONMENT CONFIGURATION FOR VICTOR RIVAS
+==========================================
+
+This is just my environment configuration. It is intended for
+ubuntu-based distros. Hope you like it.
+
+**Installation**
+
+Get the installation file
+`wget `
+
+Give it permissions
+`sudo chmod +x install.sh`
+
+Run it
+`./install.sh`
---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# Install basic stuff
+sudo apt-get install vim
+sudo apt-get install tmux
+sudo apt-get install nodejs
+sudo apt-get install git
+
+# Dev tools
+sudo apt-get install npm
+sudo npm install -g nodemon
+
+# Clone dotfiles repo
+git clone https://github.com/vctr90/dotfiles.git
+
+# Configuration files
+cp -frv dotfiles/.vim/ ~/
+cp -fv dotfiles/.vimrc ~/
+cp -frv dotfiles/.tmux/ ~/
+cp -fv dotfiles/.tmux.conf ~/
+cp -fv dotfiles/.bashrc ~/
+cp -frv dotfiles/.fluxbox/ ~/
+
+# Scripts
+cp -fv dotfiles/scripts/vim-helpers/findAll.sh ~/
+cp -fv dotfiles/scripts/vim-helpers/openAllFilesInCoincidence.sh ~/
+cp -fv dotfiles/scripts/vim-helpers/replaceAllInDirectory.sh ~/
+# Work
+cp -fv dotfiles/scripts/work/gps-bixlabs-start.sh ~/
+# Special font
+cp -fv dotfiles/scripts/install_monaco_font.sh ~/
+
+# Give permissions to run scripts
+sudo chmod +x ~/findAll.sh 
+sudo chmod +x ~/openAllFilesInCoincidence.sh
+sudo chmod +x ~/replaceAllInDirectory.sh
+sudo chmod +x ~/gps-bixlabs-start.sh
+sudo chmod +x ~/install_monaco_font.sh
+
+# Create work directories
+mkdir ~/programming/
+mkdir ~/programming/git/
+mkdir ~/programming/git/repos/
+mkdir ~/programming/git/bixlabs/
+
+# Move dotfiles folder into correct repos directory
+mv -fv dotfiles ~/programming/git/repos/
+
+# Make node name available on the path as nodejs
+sudo ln -s "$(which nodejs)" /usr/bin/node
+
+sudo reboot
